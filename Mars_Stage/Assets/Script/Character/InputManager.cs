ff9e6d2ee2ddc4c45228440fcdde5501501b922e
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-05-28 16:13:24 +0200

    progress rotate player

--- Mars_Stage/Assets/Script/Character/InputManager.cs
+++ Mars_Stage/Assets/Script/Character/InputManager.cs
@@ -8,28 +8,25 @@ public class InputManager : MonoBehaviour
     private PlayerInput _playerInput;
     private PlayerController _playerController;
     private InputAction _moveActions;
-    private Rigidbody _rb;
-    [SerializeField]private float _speed;
+    // [SerializeField] private float _speed;
+    // private Quaternion _lastRotation;
 
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
         _playerInput = GetComponent<PlayerInput>();
         _moveActions = _playerInput.actions.FindAction("Move");
-        _rb = GetComponent<Rigidbody>();
-
     }
 
     public void OnMove()
     {
-        
         Vector2 direction = _moveActions.ReadValue<Vector2>();
         Vector3 directionConvert = new Vector3(direction.x, 0f, direction.y);
-        _rb.velocity = directionConvert * Time.fixedDeltaTime * _speed;
+        // Vector3 velocity = directionConvert * _speed;
+        _playerController.Direction = directionConvert;
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionConvert), 1f);
-        
+        _playerController.ControlRotate();
 
-        // Debug.Log("test");
+
     }
 }
