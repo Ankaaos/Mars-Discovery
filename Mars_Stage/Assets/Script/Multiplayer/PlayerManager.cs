de4001e89e266bd96ea0b00793aca923f80e5b4f
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-07-04 12:40:38 +0200

    add touch scan and GamepadEnter Manager

---
+++ Mars_Stage/Assets/Script/Multiplayer/PlayerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private GamepadManager _gamepadManger;
+
+    
+    
+    public void Start()
+    {
+        
+
+    }
+
+    public void testPlayer()
+    {
+        // if (_gamepadManger.Player1Gamepad == true)
+        // {
+        //     Debug.Log("Player 1");
+        // }
+
+        // if (_gamepadManger.Player2Gamepad == false)
+        // {
+        //     Debug.Log("Player 2");
+        // }
+
+    }
+}
