0f533191288cbc0296651ba9b8091b7fca55e51b
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-07-05 16:50:28 +0200

    in progress Shader Scanner

---
+++ Mars_Stage/Assets/Script/Character_(Rover)/ScannerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerController : MonoBehaviour
+{
+    [Header("Speed")] 
+    [SerializeField] private float _speed;
+
+    [Header("Destroy Time")] 
+    [SerializeField] private float _delayDestroy;
+
+    private void Start()
+    {
+        DestroyObject();
+    }
+
+    private void Update()
+    {
+        Vector3 vectorMesh = this.transform.localScale;
+        float growing = this._speed * Time.deltaTime;
+        this.transform.localScale = new Vector3(vectorMesh.x + growing, vectorMesh.y + growing, vectorMesh.z + growing);
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(this.gameObject, _delayDestroy);
+    }
+}
