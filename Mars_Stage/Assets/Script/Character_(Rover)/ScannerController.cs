0ec468276b01d36be0f5aadc65f782583cf5c10a
Waignier Stéphane <97592098+Stephwgr@users.noreply.github.com>
2023-07-06 19:31:03 +0200

    shader detection rock OK && outline opacity in progress

--- Mars_Stage/Assets/Script/Character_(Rover)/ScannerController.cs
+++ Mars_Stage/Assets/Script/Character_(Rover)/ScannerController.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class ScannerController : MonoBehaviour
 {
-    [Header("Speed")]
+    [Header("| Scanner |")]
     [SerializeField] private float _speed;
-
-    [Header("Destroy Time")]
     [SerializeField] private float _delayDestroy;
+    public bool _destroy = false;
+
+    [Header("Script")]
+    [SerializeField] private Shader_Manager _shaderManager;
+
+
+
+
+    private void Awake()
+    {
+        _shaderManager = FindObjectOfType<Shader_Manager>();
+
+    }
 
     private void Start()
     {
@@ -20,18 +32,30 @@ public class ScannerController : MonoBehaviour
         Vector3 vectorMesh = this.transform.localScale;
         float growing = this._speed * Time.deltaTime;
         this.transform.localScale = new Vector3(vectorMesh.x + growing, vectorMesh.y + growing, vectorMesh.z + growing);
+
+        Debug.Log(_delayDestroy);
+
+
     }
 
     private void OnTriggerEnter(Collider col)
-    {   
-        if(col.gameObject.CompareTag("Rock"))
+    {
+        if (col.gameObject.CompareTag("Rock"))
         {
-            Debug.Log("Rock colision");
+            _shaderManager._opacity = 1;
+            _destroy = true;
+
         }
+
     }
+
 
     public void DestroyObject()
     {
+        
         Destroy(this.gameObject, _delayDestroy);
+
     }
+
+
 }
---
+++ Mars_Stage/Assets/Script/Shader/Shader_Manager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderManager : MonoBehaviour
+{
+    [Header("| Outline |")]
+    [SerializeField] private Material _mat;
+    private float _matF;
+    public float _opacity;
+
+    private void Start()
+    {   
+        _opacity = 0;
+        UpdateOpcaticy();
+    }
+
+    private void Update()
+    {
+        UpdateOpcaticy();
+    }
+
+    public void UpdateOpcaticy()
+    {
+        _mat.SetFloat("_Opacity", _opacity);
+
+    }
+
+}
